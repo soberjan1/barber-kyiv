5b8d889fd9bcda8e4fda63fbc1b51ad77d276c00
Kamilucha <120063185+Kamilucha@users.noreply.github.com>
2023-11-22 12:25:43 +0000

    Deploying to gh-pages from @ soberjan1/barber_kyiv@b6af78dd3bd644b29e3a79c7414217f0e92b2ed3 🚀

---
+++ index.9b6aa401.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".scroll-link").forEach((function(e){e.addEventListener("click",(function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})}))}));
+//# sourceMappingURL=index.9b6aa401.js.map
