f8fa78957e2716f1c8ab4298274a75a9394d92f5
Kamilucha <brnkamila2@gmail.com>
2023-11-22 16:33:00 +0200

    upd

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,22 +3,10 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    scrollLinks: document.querySelectorAll('.scroll-link'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  refs.scrollLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      toggleModal();
-
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
