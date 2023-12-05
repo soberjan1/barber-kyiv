f435a79d7dca458c2f35759df4d68c6f451e1a7d
soberjan1 <121058093+soberjan1@users.noreply.github.com>
2023-12-05 22:00:18 +0000

    Deploying to gh-pages from @ soberjan1/barber_kyiv@6c86d9586963c5e2488c92b0e869729d3ce2b7d8 🚀

--- index.9b6aa401.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".scroll-link").forEach((function(e){e.addEventListener("click",(function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})}))}));
-//# sourceMappingURL=index.9b6aa401.js.map
---
+++ index.ab920832.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".scroll-link").forEach(e=>{e.addEventListener("click",function(e){e.preventDefault(),document.querySelector(this.getAttribute("href")).scrollIntoView({behavior:"smooth"})})});
+//# sourceMappingURL=index.ab920832.js.map
